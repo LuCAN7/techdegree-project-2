0c9243032cb8b0572a39c9c99e2b1234523b6f4f
Lucan F <39634600+LuCAN7@users.noreply.github.com>
2019-06-02 18:34:12 -0400

    added functioning pagination links

--- js/script.js
+++ js/script.js
@@ -6,24 +6,20 @@ FSJS project 2 - List Filter and Pagination
 const ul = document.getElementsByTagName('ul');
 const list = document.getElementsByClassName('student-item');
 let page = 1; // Hard code page number which will be dynamically changed later
-const numPages = Math.ceil(list.length / 10);
 const itemsPerPage = 10; //Math.round(list.length / numPages);
+const numPages = Math.ceil(list.length / itemsPerPage);
+
 
 const showPage = (list, page) => {
-  
    const lists = Array.from(list);
    const start = (page * itemsPerPage) - itemsPerPage;
    const end = page * itemsPerPage - 1;
 
-   for (let i = 0; i < list.length; i++) {
-     
+   for (let i = 0; i < list.length; i++) { 
       lists[i].style.display = 'none';
       if (lists.indexOf(lists[i]) >= start && lists.indexOf(lists[i]) <= end) {
          lists[i].style.display = 'block';
-         // console.log('This is index: ' + lists.indexOf(lists[i]) + ' of ' + lists[i]);
-         console.log(lists[i]);
-      }
-     
+      } 
    }
 
 }
@@ -34,9 +30,46 @@ showPage(list, page);
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
-***/
+ ***/
 
-// const appendPageLinks = () => {};
+const appendPageLinks = (list) => {
+   const paginationDiv = document.createElement('div');
+   const paginationLinks = document.createElement('ul');
+   const pageDiv = document.querySelector('.page');
+   paginationDiv.className = 'pagination';
+   pageDiv.appendChild(paginationDiv);
+   paginationDiv.appendChild(paginationLinks);
+   
+   for (let i = 1; i < numPages + 1; i++) {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+    
+      paginationLinks.appendChild(li);
+      li.appendChild(a);
+      a.setAttribute('href', '#')
+      a.textContent = i;
+      
+     
+         // for(let i = 0; i < list.length; i++){
+         // //    if(){
+            
+         // //    }
+         // a.addEventListener('click', (e) => {
+         //    page = parseInt(e.target.textContent);
+         //    showPage(list, page);
+            
+         //    console.log(li.className);
+         // });
+        
 
+         // }
+      
+      
+    
+   }
+      paginationLinks.firstElementChild.firstElementChild.className = 'active'
+    
+}
 
+appendPageLinks(list);
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
