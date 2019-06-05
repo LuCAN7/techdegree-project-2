795a2ab1d252788045b656c40849d4473ff87197
Lucan F <39634600+LuCAN7@users.noreply.github.com>
2019-06-05 00:25:36 -0400

    added search elements and reference variables

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ FSJS project 2 - List Filter and Pagination
 
 const ul = document.getElementsByTagName('ul');
 const list = document.getElementsByClassName('student-item');
-const itemsPerPage = 10; //Math.round(list.length / numPages);
+const itemsPerPage = 10; 
 const numPages = Math.ceil(list.length / itemsPerPage);
 let page = 1; // Hard code page number which will be dynamically changed later
 
@@ -23,9 +23,6 @@ const showPage = (list, page) => {
    }
 
 }
-
-showPage(list, page);
-
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
@@ -51,20 +48,56 @@ const appendPageLinks = (list) => {
       
       paginationLinks.firstElementChild.firstElementChild.className = 'active'
      
-     a.addEventListener('click', (e) => {
+      a.addEventListener('click', (e) => {
         page = parseInt(e.target.textContent);
         showPage(list, page);
         let a = document.querySelectorAll('a');
-        
+        console.log(a);
         for (let i = 0; i < a.length; i++) {
            a[i].className = ''; 
          }
-         e.target.className = 'active';
+        e.target.className = 'active';
       });
       
    }
    
 }
 
+showPage(list, page);
 appendPageLinks(list);
+
+/*
+These are extra credit
+**********************
+*/
+const pageHeader = document.querySelector('.page-header');
+const div = document.createElement('div');
+const input = document.createElement('input');
+const button = document.createElement('button');
+const searchDiv = document.querySelector('#student-search');
+const searchBar = document.querySelector('input');
+const searchBtn = document.querySelector('button');
+
+input.setAttribute('placeholder', 'Search for students...')
+button.textContent = 'Search';
+
+pageHeader.appendChild(div);
+div.appendChild(input);
+div.appendChild(button);
+
+
+searchBtn.addEventListener('clicked', () => {
+   console.log(searchBar.value);
+
+});
+
+
+// const search = () => {
+
+
+// }
+
+
+
+
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
